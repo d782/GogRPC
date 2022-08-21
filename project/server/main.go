62e40c2c65abda1b2035b2195d4a981a0ab76ff0
d782 <diegocano66@gmail.com>
2022-08-21 15:34:18 -0500

    gRPC

---
+++ project/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+var addr string = "0.0.0.0:50051"
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatal("Failed to start server %v", err)
+	}
+
+	log.Printf("Listen on %s \n", addr)
+}
